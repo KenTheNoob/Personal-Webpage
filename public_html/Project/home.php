6376a4a027838a0ee7491185c6a4cb77d9d2e84a
Kenneth Cheng <kc97@njit.edu>
2024-04-04 19:45:20 -0400

    updating format and text

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -2,31 +2,25 @@
 require(__DIR__ . "/../../partials/nav.php");
 ?>
 <body>
-    <h1 style="margin-left: 10px;">About Me</h1>
-    <section>
-        <article>
-            <div>
-                <img  class="profile" src="images/Profile Pic.JPG" alt="Profile Picture" width=160px height=200px>
-                <p>	&emsp;&emsp;My name is Kenneth Cheng. I am a sophomore at NJIT and am majoring in
-                    computer science. I am from Roselle Park and am dorming at Laurel Hall. 
-                    So far I have learned Python and Java and hope to learn C++ for more efficient 
-                    video game development. My favorite subjects are computer science
-                    (obviously), math, and physics. This is because I find these subjects the most 
-                    useful in my everyday life. Alongside IT, I am currently taking Psychology, 
-                    General Chemistry II, Introduction to Computer Science II, and Calculus IIIA. 
-                    During my free time at NJIT, I usually play video games such as Genshin Impact 
-                    and Smash Bros or watch anime.
-                </p>
-                <p>
-                    &emsp;&emsp;While my hobbies are usually short-lived, some things I have dabbled
-                    in are ping-pong, Rubix's Cubes, and card throwing. My favorite food is dumplings
-                    and my favorite snacks are sour patches and cheez-its. I play the piano
-                    and alto-saxophone with one of my favorite songs being Megalovania.
-                    In order to stay in shape, I make sure to do pushups and sit-ups every day. I 
-                    view myself as an introvert who usually likes to avoid standing out. This is my 
-                    first online webpage, so Hello World!
-                </p>
-            </div>
-        </article>
-    </section>
+    <div>
+        <img  class="profile" src="images/Profile Pic.JPG" alt="Profile Picture" width=160px height=200px>
+        <h1 style="margin-left: 10px; text-align: center">About Me</h1>
+        <p>	&emsp;&emsp;My name is Kenneth Cheng. I am a junior at NJIT and am majoring in
+            computer science. I am from Roselle Park and am dorming at Laurel Hall. 
+            So far I have learned Python, Java, C, and C++ for more efficient 
+            video game development. My favorite subjects are computer science, math, and physics. This is because I find these subjects the most 
+            useful in my everyday life. Alongside IT, I am currently taking Psychology, 
+            General Chemistry II, Introduction to Computer Science II, and Calculus IIIA. 
+            During my free time at NJIT, I usually play video games such as Genshin Impact 
+            and Smash Bros or watch anime.
+        </p>
+        <p>
+            &emsp;&emsp;While my hobbies are usually short-lived, some things I have dabbled
+            in are ping-pong, Rubix's Cubes, and card throwing. My favorite food is dumplings
+            and my favorite snacks are sour patches and cheez-its. I play the piano
+            and alto-saxophone with one of my favorite songs being Megalovania.
+            In order to stay in shape, I make sure to do pushups and sit-ups every day. This is my 
+            first online webpage, so Hello World!
+        </p>
+    </div>
 </body>
