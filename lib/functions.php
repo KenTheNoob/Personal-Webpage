cb199eb47194c34c2456652211bb8574e0867052
Kenneth Cheng <kc97@njit.edu>
2024-04-04 16:42:38 -0400

    base files

---
+++ lib/functions.php
@@ -0,0 +1,4 @@
+<?php
+$BASE_PATH = '/Project';
+require(__DIR__ . "/get_url.php");
+?>
---
+++ partials/nav.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <?php require_once(__DIR__ . "/../lib/functions.php"); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
+    <script src="<?php echo get_url('helpers.js'); ?>"></script>
+    <nav>
+        <ul>
+            <div class="d-flex justify-content-around">
+                <li><a href="https://docs.google.com/document/d/1U9YXgXOIg8ATaIhlN8zc-_5QOHA3ZhCp9t-QbDeP8wI/edit?usp=sharing">Resume</a></li>
+                <li><a href="https://www.linkedin.com/in/kenneth-cheng-a55521263/">LinkedIn</a></li>
+                <li><a href="https://github.com/KenTheNoob?tab=repositories">GitHub</a></li>
+            </div>
+        </ul>
+    </nav>
+</head>
